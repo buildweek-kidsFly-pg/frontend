535eda2c8f6a86cb172321bbe369b3222184485a
Cristina Edens <edens.cristina@gmail.com>
2020-02-03 14:08:26 -0500

    set up components for Login

---
+++ src/components/login.js
@@ -0,0 +1,31 @@
+import React  from "react";
+
+
+const LogIn = () => {
+  const handleSubmit = event => {
+    event.perventDefault();
+  }
+
+
+return (
+  <div className="login">
+    <form onSubmit={handleSubmit}>
+      <h2>Log In</h2>
+      <input
+      name="username"
+      type="text"
+      placeholder="Username"
+      />
+      <input
+      name="password"
+      type="password"
+      placeholder="password"
+      />
+      <button type="submit">Log In</button>
+      <Link className="link" to="/signup">Don't have an account? Register here</Link>
+    </form>
+  </div>
+)
+}
+
+export default LogIn
