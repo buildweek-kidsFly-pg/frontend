235b4263af748df08f9cdfd23857a599dd67be96
Cristina Edens <edens.cristina@gmail.com>
2020-02-06 13:36:29 -0500

    Trips page styling completed

--- src/components/AddTrip.js
+++ src/components/AddTrip.js
@@ -2,25 +2,36 @@ import React from "react";
 
 const AddTrip = () => {
   return (
-    <div>
-      <h1>Set Up your Trip!</h1>
+    <div className="trips">
+      <h2>Set Up your Trip!</h2>
       <form>
         <label>Name your trip</label>
+        <br/>
         <input
           type="text"
           name="trip_name"
           placeholder="Add name of the trip"
         />
+        <br/>
         <label>How many kids are travelling with you?</label>
+        <br/>
         <input type="number" name="kids_traveling" />
-        <label>How many checked bugs you'll be having?</label>
+        <br/>
+        <label>How many checked bags you'll be having?</label>
+        <br/>
         <input type="number" name="checked_bags" />
-        <label>How many carryon bugs?</label>
+        <br/>
+        <label>How many carryon bags?</label>
         <input type="number" name="carryon_bags" />
+        <br/>
         <label>Carseats needed</label>
+        <br/>
         <input type="number" name="carseats" />
+        <br/>
         <label>Strollers needed</label>
+        <br/>
         <input type="number" name="strollers" />
+        <br/>
         <textarea name="notes">Anything else we should know?</textarea>
       </form>
     </div>
